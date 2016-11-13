ff7ff33a1085dda75c94d8ebd26c47a30b637920
rhallmark <russellhallmark@gmail.com>
2016-11-12 19:59:54 -0600

    Login Page Updated -- Needs DB Integration

--- app/wpSignLocked/wpSignLocked.component.js
+++ app/wpSignLocked/wpSignLocked.component.js
@@ -10,6 +10,43 @@ var __metadata = (this && this.__metadata) || function (k, v) {
 };
 const core_1 = require('@angular/core');
 let SignLocked = class SignLocked {
+    constructor() {
+        this.pwconf = false;
+        this.login_text = "Submit";
+        this.signup_text = "Sign Up";
+    }
+    login() {
+        if (this.login_text === "Submit") {
+            this.submit_login();
+        }
+        else {
+            this.showLoginForm();
+        }
+    }
+    submit_login() {
+        //database stuff goes here
+    }
+    signup() {
+        if (this.signup_text === "Submit") {
+            this.submit_signup();
+        }
+        else {
+            this.showSignUpForm();
+        }
+    }
+    submit_signup() {
+        //database stuff goes here
+    }
+    showLoginForm() {
+        this.pwconf = false;
+        this.login_text = "Submit";
+        this.signup_text = "Sign Up";
+    }
+    showSignUpForm() {
+        this.pwconf = true;
+        this.login_text = "Login Form";
+        this.signup_text = "Submit";
+    }
 };
 SignLocked = __decorate([
     core_1.Component({
--- app/wpSignLocked/wpSignLocked.component.ts
+++ app/wpSignLocked/wpSignLocked.component.ts
@@ -6,5 +6,53 @@ import { Component } from '@angular/core';
     styleUrls: [ 'app/wpSignLocked/wpSignLocked.css' ]
 })
 export class SignLocked {
-    
+
+login_text: string;
+signup_text: string;    
+pwconf: boolean;
+
+    constructor(){
+        this.pwconf = false;
+        this.login_text = "Submit"
+        this.signup_text = "Sign Up"
+    }
+
+    login() {
+        if(this.login_text === "Submit"){
+            this.submit_login();
+        }
+        else{
+            this.showLoginForm();
+        }
+    }
+
+    submit_login() {
+        //database stuff goes here
+    }
+
+    signup() {
+        if(this.signup_text === "Submit"){
+            this.submit_signup();
+        }
+        else{
+            this.showSignUpForm();
+        }
+    }
+
+    submit_signup() {
+        //database stuff goes here
+    }
+
+    showLoginForm() {
+        this.pwconf = false;
+        this.login_text = "Submit"
+        this.signup_text = "Sign Up"
+    }
+
+    showSignUpForm() {
+        this.pwconf = true;
+        this.login_text = "Login Form"
+        this.signup_text = "Submit"
+    }
+
 }
